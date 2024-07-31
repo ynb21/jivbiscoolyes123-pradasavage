1481cce48397db0a484315ad2ab8b625f22d03a3
risk <jivb@onionmail.org>
2024-07-31 13:16:03 -0400

    vulify.lol

--- dns/effects/cursor.js
+++ dns/effects/cursor.js
@@ -13,7 +13,7 @@ setInterval(function() {
     sparkle.style.opacity = 1;
     document.body.appendChild(sparkle);
     sparkle.style.transition = 'opacity 0.5s ease-out, transform 0.5s ease-out';
-    sparkle.style.transform = `translate(-50%, -50%) scale(${Math.random() * 1.5 + 0.5})`;
+    sparkle.style.transform = `translate(-50%, -50%) scale(${Math.random() * 1.5 + 1.5})`;
     sparkle.style.animation = `sparkleAnimation ${Math.random() * 0.5 + 0.5}s ease-out`;
     setTimeout(() => {
         sparkle.remove();
