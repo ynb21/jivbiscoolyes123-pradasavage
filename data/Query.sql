09026ac34a4f981259297140e9909693de0e1d48
risk <jivb@onionmail.org>
2024-08-25 09:30:43 -0400

    vulify.lol

--- data/Query.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM view_count WHERE id = 1;
-INSERT INTO view_count (id, count) VALUES (1, 0);
--- data/db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE webpage;
-USE webpage;
-CREATE TABLE view_count (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    count INT NOT NULL
-);
-INSERT INTO view_count (count) VALUES (0);
